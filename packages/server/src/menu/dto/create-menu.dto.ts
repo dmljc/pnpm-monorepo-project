d09a77b43557014650c0d12d8422ab4b5baf6d0f
zfc <17602143402@163.com>
2025-07-12 12:25:45 +0800

    refactor(menu): 移除菜单实体并简化DTO验证
    删除不再使用的Menu实体类及相关序列化逻辑
    在CreateMenuDto中增加字符串类型验证

--- packages/server/src/menu/dto/create-menu.dto.ts
+++ packages/server/src/menu/dto/create-menu.dto.ts
@@ -1,9 +1,13 @@
-import { IsNotEmpty, IsOptional } from "class-validator";
+import { IsString, IsNotEmpty, IsOptional } from "class-validator";
 
 export class CreateMenuDto {
+    // 新增 @IsNotEmpty 验证非空
+    @IsString({ message: "菜单类型必须是字符串" })
     @IsNotEmpty({ message: "菜单类型不能为空" })
     type: string;
 
+    // 新增 @IsNotEmpty 验证非空
+    @IsString({ message: "菜单名称必须是字符串" })
     @IsNotEmpty({ message: "菜单名称不能为空" })
     name: string;
 
--- packages/server/src/menu/menu.controller.ts
+++ packages/server/src/menu/menu.controller.ts
@@ -11,21 +11,6 @@ import { RequireLogin } from "../common/custom-decorator";
 import { MenuService } from "./menu.service";
 import { CreateMenuDto } from "./dto/create-menu.dto";
 import { UpdateMenuDto } from "./dto/update-menu.dto";
-import { Menu } from "./entities/menu.entity";
-
-function serializeMenu(menu: Menu): any {
-    return {
-        id: menu.id,
-        name: menu.name,
-        type: menu.type,
-        url: menu.url,
-        icon: menu.icon,
-        code: menu.code,
-        component: menu.component,
-        parentId: menu.parent ? menu.parent.id : (menu.parentId ?? null),
-        children: menu.children ? menu.children.map(serializeMenu) : [],
-    };
-}
 
 @Controller("menu")
 export class MenuController {
@@ -40,8 +25,7 @@ export class MenuController {
     @Get("list")
     @RequireLogin()
     async findAll() {
-        const tree = await this.menuService.findAll();
-        return tree.map(serializeMenu);
+        return await this.menuService.findAll();
     }
 
     @Get(":id")
