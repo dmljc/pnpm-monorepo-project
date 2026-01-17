4443b8cbf866012d762f4618ef583f1b3eba09b5
zfc <17602143402@163.com>
2026-01-17 21:09:56 +0800

    refactor(analysis): 移除分析页面代码

--- apps/admin/src/pages/Analysis/index.tsx
+++ apps/admin/src/pages/Analysis/index.tsx
@@ -1,24 +1,11 @@
-import { FC, useEffect, useRef } from "react";
-import { ThreeApp } from "tthree";
-import { wrapperStyle, containerStyle } from "./style";
+import { FC } from "react";
+
+// import { wrapperStyle, containerStyle } from "./style";
 
 const Analysis: FC = () => {
-    const containerRef = useRef<HTMLDivElement>(null);
-
-    useEffect(() => {
-        if (!containerRef.current) return;
-
-        // 使用 containerRef 外部传入的dom元素创建 Three.js 应用，并链式调用初始化和加载模型
-        const app = new ThreeApp({ container: containerRef.current });
-        app.init().loadModel("/park.glb");
-
-        // 在组件卸载时清理资源
-        return () => app.destroy();
-    }, []);
-
     return (
-        <div style={wrapperStyle}>
-            <div ref={containerRef} style={containerStyle} />
+        <div>
+            <h1>分析页面</h1>
         </div>
     );
 };
